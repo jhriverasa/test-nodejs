9ee91b8cb5a9423f2a616b7cff18b656e20565de
jhriverasa <jhriverasa@unal.edu.co>
2023-10-01 23:50:52 -0500

    refactor(src): format all .js files

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-require('dotenv').config();
+require("dotenv").config();
 console.clear();
-require('./src');
+require("./src");
--- src/app/db/index.js
+++ src/app/db/index.js
@@ -1,25 +1,25 @@
-'use strict';
+"use strict";
 
-const Sequelize = require('sequelize');
-const models = require('./models');
+const Sequelize = require("sequelize");
+const models = require("./models");
 
 let sequelize;
 
 sequelize = new Sequelize("sqlite::memory:", {
-  logging: false //console.log
+  logging: false, //console.log
 });
 
 const db = {
-	Sequelize,
-	sequelize,
+  Sequelize,
+  sequelize,
 };
 
 for (const modelInit of models) {
-	const model = modelInit(db.sequelize, db.Sequelize.DataTypes);
-	db[model.name] = model;
+  const model = modelInit(db.sequelize, db.Sequelize.DataTypes);
+  db[model.name] = model;
 }
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
@@ -28,19 +28,18 @@ Object.keys(db).forEach(modelName => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-
 const initDB = async () => {
   await db.swPeople.sync({ force: true });
   await db.swPlanet.sync({ force: true });
   await db.logging.sync({ force: true });
-}
+};
 
 const populateDB = async () => {
   await db.swPlanet.bulkCreate([
     {
       name: "Tatooine",
-      gravity: 1.0
-    }
+      gravity: 1.0,
+    },
   ]);
   await db.swPeople.bulkCreate([
     {
@@ -48,16 +47,16 @@ const populateDB = async () => {
       height: 172,
       mass: 77,
       homeworld_name: "Tatooine",
-      homeworld_id: "/planets/1"
-    }
+      homeworld_id: "/planets/1",
+    },
   ]);
-}
+};
 
 const deleteDB = async () => {
   await db.swPeople.drop();
   await db.swPlanet.drop();
   await db.logging.drop();
-}
+};
 
 const watchDB = async () => {
   const planets = await db.swPlanet.findAll({
@@ -73,7 +72,7 @@ const watchDB = async () => {
   console.log("\n");
   console.log("============= swPeople =============");
   console.table(people);
-}
+};
 
 db.initDB = initDB;
 db.populateDB = populateDB;
